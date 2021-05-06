e5ac7f3dd9a028150616e9926488b9f00836bb48
Alex Hughes <alexhughes@AlexMBPro.local>
2021-05-06 14:29:23 -0500

    feat: add Auto Review article

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,8 @@ test('renders lMy really cool blog site logo', () => {
   const linkElement = screen.getByText(/My really cool blog site/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders Auto Review Article', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Auto Review:/i);
+  expect(linkElement).toBeInTheDocument();
+});
